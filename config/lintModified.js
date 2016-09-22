90b5e88e0a3c5eb8027a030d1fb2cae25308674a
Bohdan Huseinov <bohdan_huseinov@epam.com>
2016-09-22 16:20:52 +0300

    fixed pre-commit eslint

--- config/lintModified.js
+++ config/lintModified.js
@@ -5,21 +5,26 @@ const exec = require('child_process').exec,
 
 let ignoreList = fs.readFileSync('.eslintignore', 'utf-8').split('\n');
 
-exec('git ls-files --modified', (err, stdout, stderr) => {
-    let modified = stdout.split('\n');
+exec('git diff --cached --name-status --diff-filter=ACM', (err, stdout, stderr) => {
+    let modified = stdout.split('\n').map((file) => {
+        return file.slice(1).trim();
+    });
 
     modified = modified.filter((file) => {
         return /\.js$/.test(file) && ignoreList.indexOf(file) === -1;
     }).join(' ');
 
     console.log('modified files:', modified);
+    if(modified.indexOf('.js') === -1) {
+        return;
+    }
 
     exec(`eslint ${modified}`, (err, stdout, stderr) => {
         console.log(stdout);
         let nmbOfErrors = /\d+?(?= error)/m.exec(stdout);
 
         if(nmbOfErrors && parseInt(nmbOfErrors[0]) > 0) {
-            throw new Error('You\'ve got some eslint issues in your files');
+            process.exit(1);
         }
         console.log(stderr);
     });
