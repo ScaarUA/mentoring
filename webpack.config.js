ba752cc99760219277dfd58c34d76dba94096506
Bohdan Huseinov <bohdan_huseinov@epam.com>
2016-10-20 17:13:03 +0300

    Responsive design, added font-awesome

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,21 @@ let baseConfig = {
             {
                 test: /\.html$/,
                 loader: 'raw'
+            }, {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            }, {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            }, {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            }, {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            }, {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
             }
         ]
     },
