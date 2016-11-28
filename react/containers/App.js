1730d39ae99ea784cf00b817fd5792575e9d7c9b
Vitalii Kotliar <Vitalii Kotliar>
2016-11-28 23:28:16 +0200

    fix react

--- react/containers/App.js
+++ react/containers/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
 
-
-class App extends Component {
+export default class App extends Component {
     render() {
         return (
             <div className="container">
@@ -14,14 +10,4 @@ class App extends Component {
             </div>
         )
     }
-}
-
-function mapStateToProps(state) {
-    return { data: state }
-}
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actions, dispatch)
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+};
